ef05527ed30c19c72001c1d63b5f84ab6d4859ab
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-09-16 11:42:10 -0700

    Stupid value mismatch.

--- app/validators/gender_validator.rb
+++ app/validators/gender_validator.rb
@@ -4,12 +4,12 @@ class GenderValidator < ActiveModel::Validator
   def validate(record)
     @record = record
     @record.errors[:provider] << "not present" if @record.provider.empty?
-    @record.errors[:type] << "not a valid gender" if ungendered?
+    @record.errors[:value] << "not a valid gender" if ungendered?
   end
 
   private
 
   def gendered?
-    !VALID_GENDERS.cover?(@record.type)
+    !VALID_GENDERS.cover?(@record.value)
   end
 end
--- db/migrate/20130910184922_create_genders.rb
+++ db/migrate/20130910184922_create_genders.rb
@@ -1,7 +1,7 @@
 class CreateGenders < ActiveRecord::Migration
   def change
     create_table :genders do |t|
-      t.integer :type, index: true
+      t.integer :value, index: true
       t.float :confidence, default: 0.0
       t.string :type, index: true
       t.text :data
