9e685519ce02f95b7e874a8f23c7215bb6d1bf0a
Tyler Morgan <tylermorgan86@gmail.com>
2013-12-06 11:25:56 -0800

    more daemon options work

--- lib/remotus/remote/twitter_stream.rb
+++ lib/remotus/remote/twitter_stream.rb
@@ -15,7 +15,7 @@ module Remotus
        config.oauth_token = ENV["TWITTER_ACCESS_KEY"]
        config.oauth_token_secret = ENV["TWITTER_ACCESS_SECRET"]
      end
-     TweetStream::Daemon.new('twitter_stream', {multiple: true, ontop: true })
+     TweetStream::Daemon.new('twitter_stream', :ontop => true)
     end
   end
 end
