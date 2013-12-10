508e24913c5998b1a06273e9f06ec40690d8de2f
Tyler Morgan <tylermorgan86@gmail.com>
2013-12-09 17:08:07 -0800

    added some debugging to confirm status data updates

--- app/workers/stream_search_worker.rb
+++ app/workers/stream_search_worker.rb
@@ -12,7 +12,9 @@ class StreamSearchWorker
     warn("results form back")
     if @result.valid?
       @result.save
+      warn "Updating search_statuses_counts for search_id: #{@search.id}"
       UpdateSearchesStatusesCountWorker.perform_async(@search.id)
+      warn "Creating person for profile_id: #{@result.profile.id}"
       FetchPersonWorker.perform_async(@result.profile.id)
     end
   end
--- lib/remotus/version.rb
+++ lib/remotus/version.rb
@@ -1,3 +1,3 @@
 module Remotus
-  VERSION = "1.4.10"
+  VERSION = "1.4.11"
 end
