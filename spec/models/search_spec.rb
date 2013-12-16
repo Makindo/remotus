38f088a0c543dd1343273be3a0bdb34cbf5ad107
Tyler Morgan <tylermorgan86@gmail.com>
2013-12-16 10:05:17 -0800

    fixing regex generation with tests

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -3,13 +3,22 @@ require "spec_helper"
 describe Search do
   before do 
     @search = Search.new(type: "TwitterSearch", query: "a Search")
+    @expected_regex = /(.*)(a) (.*)(search)( |$|\.)(.*)/
   end
 
   it "Should be able to convert the query to a regex" do
-    expect(@search.to_regex).to eql(/(.*)(a) (.*)(search)(.*)/)
+    expect(@search.to_regex).to eql(@expected_regex)
   end
 
   it "should be able to create a query hash for stream searches" do
-    expect(@search.query_regex[1]).to eql(/(.*)(a) (.*)(search)(.*)/)
+    expect(@search.query_regex[1]).to eql(@expected_regex)
+  end
+
+  it "should match a correct status" do
+    expect(!!("this is a search".match(@search.to_regex))).to be_true
+  end
+
+  it "shouldn't match an incorrect status" do
+    expect(!!("this a bad searching".match(@search.to_regex))).to_not be_true
   end
 end
--- spec/remotes/twitter_stream_remote_spec.rb
+++ spec/remotes/twitter_stream_remote_spec.rb
@@ -4,12 +4,13 @@ describe TwitterStreamRemote do
   before do
     @search = Search.new(type: "TwitterSearch", query: "a test query", active: true)
     @search.save
+    @expected_regex = /(.*)(a) (.*)(test) (.*)(query)( |$|\.)(.*)/
   end
   
   it "should be able to create a new remote with query regexs" do
     @remote = TwitterStreamRemote.new([], 1)
     expect(@remote.instance_variable_get(:@regexs)).to_not be_blank
     regexs  = @remote.instance_variable_get(:@regexs)
-    expect(regexs[@search.id]).should == /(.*)(a) (.*)(test) (.*)(query)(.*)/
+    expect(regexs[@search.id]).should == @expected_regex
   end
 end
