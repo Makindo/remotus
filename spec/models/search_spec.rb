f34392b6b0de730ac7e9f56e4deace3c10cc4516
Tyler Morgan <tylermorgan86@gmail.com>
2013-12-31 09:16:09 -0800

    still trying to fix this same damn issue

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,24 +1,40 @@
 require "spec_helper"
 
 describe Search do
-  before do 
-    @search = Search.new(type: "TwitterSearch", query: "a Search")
-    @expected_regex = /(.*)(a) (.*)(search)( |$|\.)(.*)/
+  context "valid search and regexs" do
+    before do 
+      @search = Search.new(type: "TwitterSearch", query: "a Search")
+      @expected_regex = /(.*)(a) (.*)(search)( |$|\.)(.*)/
+    end
+
+    it "Should be able to convert the query to a regex" do
+      expect(@search.to_regex).to eql(@expected_regex)
+    end
+
+    it "should be able to create a query hash for stream searches" do
+      expect(@search.query_regex[1]).to eql(@expected_regex)
+    end
+
+    it "should match a correct status" do
+      expect(!!("this is a search".match(@search.to_regex))).to be_true
+    end
+
+    it "shouldn't match an incorrect status" do
+      expect(!!("this a bad searching".match(@search.to_regex))).to_not be_true
+    end
   end
 
-  it "Should be able to convert the query to a regex" do
-    expect(@search.to_regex).to eql(@expected_regex)
-  end
+  context "two searches of different types" do
+    before do
+      @search = Search.new(type: "TwitterSearch", query: "a test search")
+      @search2 = Search.new(type: "TwitterStreamSearch", query: "a test search")
+    end
 
-  it "should be able to create a query hash for stream searches" do
-    expect(@search.query_regex[1]).to eql(@expected_regex)
-  end
-
-  it "should match a correct status" do
-    expect(!!("this is a search".match(@search.to_regex))).to be_true
-  end
-
-  it "shouldn't match an incorrect status" do
-    expect(!!("this a bad searching".match(@search.to_regex))).to_not be_true
+    it "be able to have the same query with different types" do
+      @search.save
+      expect(@search).to be_valid
+      @search2.save
+      expect(@search2).to be_valid
+    end
   end
 end
