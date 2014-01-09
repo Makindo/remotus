abfabfaa743684946e675aeb8e00a6f4d4e3bbc2
Tyler Morgan <tylermorgan86@gmail.com>
2014-01-08 16:29:37 -0800

    profile dependencies

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,7 @@ class Profile < ActiveRecord::Base
   include Remotus::Provider
 
   has_many :statuses, dependent: :destroy
-  has_many :geolocations, as: :source
+  has_many :geolocations, as: :source, dependent: :destroy
 
   validates_with ProfileValidator
 
