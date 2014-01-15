003a1ce29218e4c1a556390f0e3a72ec3ba7c4a7
tylermorgan86 <tylermorgan86@gmail.com>
2014-01-15 15:50:11 -0800

    fixing an app error

--- app/remotes/twitter_gnip_remote.rb
+++ app/remotes/twitter_gnip_remote.rb
@@ -3,19 +3,10 @@ class TwitterGnipRemote
   include Remotus::RemoteGnip
 
   def initialize
-    @search = Remotus::RemoteGnip.search_client
-    @rules = Remotus::RemoteGnip.rules_client
-    @stream = Remotus::RemoteGnip.stream_client
+
   end
 
   def build_rules
-    @accounts = Account.all
-    @account.each do |account| 
-      account.geolocations.each do |geolocation|
-        @account.searches.each do |search|
-          
-        end
-      end
-    end
+
   end
 end
