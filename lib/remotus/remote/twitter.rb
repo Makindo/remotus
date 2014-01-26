5ae582561244895c7e69e33b90d3604a8f34bb46
tylermorgan86 <tylermorgan86@gmail.com>
2014-01-26 11:02:52 -0800

    updating to comply with changes to the twitter gem

--- lib/remotus/remote/twitter.rb
+++ lib/remotus/remote/twitter.rb
@@ -1,11 +1,11 @@
 module Remotus
   module RemoteTwitter
     def self.client
-      Twitter::Client.new(
+      Twitter::REST::Client.new(
         :consumer_key => ENV["TWITTER_API_KEY"],
         :consumer_secret => ENV["TWITTER_API_SECRET"],
-        :oauth_token => ENV["TWITTER_ACCESS_KEY"],
-        :oauth_token_secret => ENV["TWITTER_ACCESS_SECRET"])
+        :auth_token => ENV["TWITTER_ACCESS_KEY"],
+        :auth_token_secret => ENV["TWITTER_ACCESS_SECRET"])
     end
   end
 end
--- lib/remotus/version.rb
+++ lib/remotus/version.rb
@@ -1,3 +1,3 @@
 module Remotus
-  VERSION = "1.9.17"
+  VERSION = "1.10.0"
 end
