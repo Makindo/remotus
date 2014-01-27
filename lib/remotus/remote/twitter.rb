f1aed576470814b473279787ca48dfc1159a477a
tylermorgan86 <tylermorgan86@gmail.com>
2014-01-26 16:09:15 -0800

    fix in remote/twitter.rb

--- lib/remotus/remote/twitter.rb
+++ lib/remotus/remote/twitter.rb
@@ -2,10 +2,10 @@ module Remotus
   module RemoteTwitter
     def self.client
       Twitter::REST::Client.new do |config| 
-        :consumer_key => ENV["TWITTER_API_KEY"],
-        :consumer_secret => ENV["TWITTER_API_SECRET"],
-        :auth_token => ENV["TWITTER_ACCESS_KEY"],
-        :auth_token_secret => ENV["TWITTER_ACCESS_SECRET"]
+        config.consumer_key => ENV["TWITTER_API_KEY"],
+        config.consumer_secret => ENV["TWITTER_API_SECRET"],
+        config.auth_token => ENV["TWITTER_ACCESS_KEY"],
+        config.auth_token_secret => ENV["TWITTER_ACCESS_SECRET"]
       end
     end
   end
