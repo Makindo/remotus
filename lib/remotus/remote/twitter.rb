48925d44793ddd4d151771019ae3532b7cb52d20
Tyler Morgan <tylermorgan86@gmail.com>
2014-01-30 09:05:47 -0800

    remote twitter change

--- lib/remotus/remote/twitter.rb
+++ lib/remotus/remote/twitter.rb
@@ -4,8 +4,8 @@ module Remotus
       Twitter::REST::Client.new do |config| 
         config.consumer_key = ENV["TWITTER_API_KEY"]
         config.consumer_secret = ENV["TWITTER_API_SECRET"]
-        config.auth_token = ENV["TWITTER_ACCESS_KEY"]
-        config.auth_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+        config.access_token = ENV["TWITTER_ACCESS_KEY"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
       end
     end
   end
--- lib/remotus/version.rb
+++ lib/remotus/version.rb
@@ -1,3 +1,3 @@
 module Remotus
-  VERSION = "1.10.10"
+  VERSION = "1.10.11"
 end
